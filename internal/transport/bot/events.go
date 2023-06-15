5af584cbe02e8d3c0ce84fcf5d667d5fa4f08fe5
mrfreemanhr <mrfreemanhr@gmail.com>
2023-06-15 18:30:48 +0300

    TelegramAPI lib messages processing;

--- internal/transport/bot/events.go
+++ internal/transport/bot/events.go
@@ -2,6 +2,7 @@ package bot
 
 import (
 	"adblock_bot/internal/adapter/logger"
+	"adblock_bot/internal/core/entity"
 
 	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
 )
@@ -13,12 +14,49 @@ func (b *bot) ProcessEvents(event *tgbotapi.Update) {
 }
 
 func (b *bot) OnMessage(event *tgbotapi.Update) {
-	logger.Logger().UselessInfo("Message: %s", event.Message.Text)
-
-	for _, handler := range b.messageHandlers {
-		processed := handler.ProcessMessage(event)
-		if processed {
-			break
+	msg, err := b.processMessageEntity(event)
+	if err != nil {
+		logger.Logger().UselessInfo("Message: %s", msg.Text)
+		for _, handler := range b.messageHandlers {
+			processed := handler.ProcessMessage(nil)
+			if processed {
+				break
+			}
 		}
 	}
+}
+
+func (b *bot) processMessageEntity(message *tgbotapi.Update) (entity.TelegramMessage, error) {
+	var messageEntity entity.TelegramMessage
+	messageEntity.Text = message.Message.Text
+	messageEntity.Audio = message.Message.Audio
+	messageEntity.Photo = message.Message.Photo
+	messageEntity.VideoNote = message.Message.VideoNote
+	messageEntity.MessageID = int64(message.Message.MessageID)
+	// Sender processing
+	messageEntity.From = &entity.TelegramUser{
+		ID:           message.Message.From.ID,
+		IsBot:        message.Message.From.IsBot,
+		FirstName:    message.Message.From.FirstName,
+		LastName:     message.Message.From.LastName,
+		UserName:     message.Message.From.UserName,
+		LanguageCode: message.Message.From.LanguageCode,
+	}
+	// Chat processing
+	messageEntity.Chat = &entity.TelegramChat{
+		ID:    message.Message.Chat.ID,
+		Type:  message.Message.Chat.Type,
+		Title: message.Message.Chat.Title,
+		Permissions: &entity.TelegramChatPermissions{
+			CanSendMessages:       message.Message.Chat.Permissions.CanSendMessages,
+			CanSendMediaMessages:  message.Message.Chat.Permissions.CanSendMediaMessages,
+			CanSendPolls:          message.Message.Chat.Permissions.CanSendPolls,
+			CanSendOtherMessages:  message.Message.Chat.Permissions.CanSendOtherMessages,
+			CanAddWebPagePreviews: message.Message.Chat.Permissions.CanAddWebPagePreviews,
+			CanChangeInfo:         message.Message.Chat.Permissions.CanChangeInfo,
+			CanInviteUsers:        message.Message.Chat.Permissions.CanInviteUsers,
+			CanPinMessages:        message.Message.Chat.Permissions.CanPinMessages,
+		},
+	}
+	return messageEntity, nil
 }
